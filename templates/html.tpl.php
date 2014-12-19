7ebac70745df369137db892e2a15d80068a6d939
Michael McGrath <mmcgrath@mcgrathsolutions.org>
2014-12-19 13:00:13 -0500

    initial code

---
+++ templates/html.tpl.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+<?php print $head; ?>
+<title><?php print $head_title; ?></title>
+<?php print $styles; ?>
+<?php print $scripts; ?>
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery.cycle2/20130409/jquery.cycle2.min.js"></script>
+</head>
+<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
