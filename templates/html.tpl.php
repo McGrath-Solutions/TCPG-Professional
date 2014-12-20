8df46729144c7793ba0364184e6af2f6b374498e
Michael McGrath <mmcgrath@mcgrathsolutions.org>
2014-12-19 20:04:37 -0500

    java2cycle jquery only load for front page

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -5,8 +5,10 @@
 <?php print $styles; ?>
 <?php print $scripts; ?>
 <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery.cycle2/20130409/jquery.cycle2.min.js"></script>
+ <?php if ($is_front): ?>    
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery.cycle2/20130409/jquery.cycle2.min.js"></script>
+<?php endif; ?>
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print $page_top; ?>
